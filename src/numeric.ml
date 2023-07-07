2188920740e028e4de11e23e78a7ea8c190dabb3
Eugene Lykhovyd <lykhovyd@gmail.com>
2023-07-07 16:43:11 +0000

    for igor to debug

--- src/numeric.ml
+++ src/numeric.ml
@@ -1,3 +1,4 @@
+open Devkit
 open Problem_t
 (*type attendee = { x: float; y: float; tastes: float list }
 
@@ -13,13 +14,19 @@ type problem = {
 *)
 (* compute almost-optimal x and y for musician and all attendees, corner is likely *)
 let almost_optimal_musician inst p =
-  let atts = List.map (fun att -> att.x, att.y, (List.nth att.tastes inst)) p.attendees in (* FIXME better *)
+  let atts = List.filter_map (fun att ->
+    let t = List.nth att.tastes inst in
+    if abs_float t < 0.00001 then None else
+      Some (att.x, att.y, t)
+  ) p.attendees in (* FIXME better *)
   let sum_ti = List.fold_left (fun prev (_,_,t) -> prev +. (1. /. t)) 0. atts in
   let sum_ai_ti = List.fold_left (fun prev (a,_,t) -> prev +. (a /. t)) 0. atts in
   let sum_bi_ti = List.fold_left (fun prev (_,b,t) -> prev +. (b /. t)) 0. atts in
 
-  let x_star = sum_ti /. sum_ai_ti in
-  let y_star = sum_ti /. sum_bi_ti in
+  let x_star = sum_ai_ti /. sum_ti in
+  let y_star = sum_bi_ti /. sum_ti in
+
+  printfn "sum ti %f X STAR %f Y STAR %f" sum_ti x_star y_star;
 
   (* place inside of the stage +- 10 *)
   let stage_x_min = 10. +. (List.nth p.stage_bottom_left 0) in
