433c56b5861de6a1e675555d80dbf4f49766737d
ygrek <ygrek@autistici.org>
2023-07-07 09:39:36 -0400

    check all parse

--- src/main.ml
+++ src/main.ml
@@ -2,7 +2,10 @@ open Printf
 open Devkit
 
 let problem n = sprintf "../problem/%d" n
+let problems = 45
 
 let () =
-  let j = Problem_j.problem_of_string @@ Std.input_file @@ problem 1 in
-  printfn "%d" (List.length j.musicians)
+  for i = 1 to problems do
+    let j = Problem_j.problem_of_string @@ Std.input_file @@ problem i in
+    printfn "%d %d" (List.length j.musicians) (List.length j.attendees)
+  done
