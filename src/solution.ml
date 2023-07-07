52fe2c99f1ffe3545babd372bbdfa382f4c58176
Eugene Lykhovyd <lykhovyd@gmail.com>
2023-07-07 19:55:02 +0000

    add intersection logic

--- src/solution.ml
+++ src/solution.ml
@@ -20,7 +20,7 @@ let (-) = (-.)
 end
 
 (* https://mathworld.wolfram.com/Circle-LineIntersection.html *)
-let is_blocked ~by a b =
+(*let is_blocked ~by a b =
   let open FOP in
   let ax = fst a - fst by in
   let ay = snd a - snd by in
@@ -36,7 +36,23 @@ let is_blocked ~by a b =
       let dnt = ax * by - bx * ay in
       r2 * dr2 - sqr dnt
     in
-    incidence >= 0.
+    incidence >= 0.*)
+
+(* point 1 (x1,y1)
+   point 2 (x2,y2)
+   circle center (x0,y0) radius r
+*)
+let is_blocked (x1,y1) (x2,y2) (x0,y0,r) =
+  let a = (x2 -. x1)*.(x2 -. x1) +. (y2 -. y1)*.(y2 -. y1) in
+  let b = 2. *. ( (x2 -. x1)*.(x1 -. x0) +. (y2 -. y1)*.(y1 -. y0)) in
+  let c = (x1 -. x0)*.(x1 -. x0) +. (y1 -. y0)*.(y1 -. y0) -. r*.r in
+
+  let det = b*.b -. 4.*.a*.c in
+  if det < 0. then false else begin
+    let t1 = (-.b +. (Float.sqrt det)) /. (2.*.a) in
+    let t2 = (-.b -. (Float.sqrt det)) /. (2.*.a) in
+    ( (t1 >= 0.) && (t1 <= 1.) ) || ( (t2 >= 0.) && (t2 <= 1.) )
+  end
 
 let score p s =
   assert (List.length s.placements = Array.length p.musicians);
