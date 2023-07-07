863eed609cf98bc380bdf5e7e2e389faa85febff
ygrek <ygrek@autistici.org>
2023-07-07 15:34:16 -0400

    is_blocked wip

--- src/solution.ml
+++ src/solution.ml
@@ -1,4 +1,4 @@
-open Printf
+  open Printf
 open Problem_t
 
 let path i = sprintf "../solution/%d" i
@@ -12,6 +12,32 @@ let save i s = Std.output_file ~filename:(path i) ~text:(Problem_j.string_of_sol
 
 let sqr x = x *. x
 
+module FOP = struct
+let (+) = (+.)
+let ( * ) = ( *. )
+let (/) = (/.)
+let (-) = (-.)
+end
+
+(* https://mathworld.wolfram.com/Circle-LineIntersection.html *)
+let is_blocked ~by a b =
+  let open FOP in
+  let ax = fst a - fst by in
+  let ay = snd a - snd by in
+  let bx = fst b - fst by in
+  let by = snd b - snd by in
+  if Float.(sign_bit ax = sign_bit bx && sign_bit ay = sign_bit by) then false
+  else
+    let incidence =
+      let r2 = 25. in
+      let dx = bx - ax in
+      let dy = by - ay in
+      let dr2 = sqr dx + sqr dy in
+      let dnt = ax * by - bx * ay in
+      r2 * dr2 - sqr dnt
+    in
+    incidence >= 0.
+
 let score p s =
   assert (List.length s.placements = Array.length p.musicians);
   p.attendees
@@ -21,4 +47,4 @@ let score p s =
       1_000_000. *. a.tastes.(p.musicians.(i)) /. d2)
     |> List.fold_left (+.) 0.
   end
-  |> List.fold_left (+.) 0.
+    |> List.fold_left (+.) 0.
