ceeee3db1b322b9708e4114e7cd181f75ed839ad
Eugene Lykhovyd <lykhovyd@gmail.com>
2023-07-07 17:54:34 +0000

    fix

--- src/solver.ml
+++ src/solver.ml
@@ -2,7 +2,7 @@ open Devkit
 open Problem_t
 
 let solve p =
-  let insts = List.sort_uniq compare p.musicians in
+  let insts = List.sort_uniq compare @@ Array.to_list p.musicians in
   let _inst_centers = List.map (fun inst ->
     Numeric.almost_optimal_musician inst p
   ) insts in
